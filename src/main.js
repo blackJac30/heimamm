05e282c9b7fd9a7a39ece8f48b4646bffab05326
blackJac30 <3512472118@qq.com>
2020-03-16 22:58:37 +0800

    配置路由

--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// 导入Element-ui
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+// 使用Element-ui
+Vue.use(ElementUI);
+
+// 导入全局样式
+import './style/index.css'
+import './style/base.css'
+
 Vue.config.productionTip = false
 
+// 导入vue-router的文件
+import router from './router/index.js'
 new Vue({
   render: h => h(App),
+  router // 将路由对象挂载到Vue实例中
 }).$mount('#app')
---
+++ src/router/index.js
@@ -0,0 +1,27 @@
+// 配制路由js文件
+
+// 导入Vue
+import Vue from 'vue'
+// 导入router
+import VueRouter from 'vue-router'
+// 使用vue-router(注册路由)
+Vue.use(VueRouter)
+
+// 解决二次点击报错问题
+const routerPush = VueRouter.prototype.push
+VueRouter.prototype.push = function push(location) {
+    return routerPush.call(this, location).catch(error => error)
+}
+
+// 导入路由子组件
+import login from '../views/login/index.vue'
+
+// 创建一个新的vue-router对象
+const router = new VueRouter({
+    routes: [{
+        path: "/",
+        component: login
+    }]
+})
+// 把vue-router对象暴露出去
+export default router
